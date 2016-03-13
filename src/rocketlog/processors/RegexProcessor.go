32f16be818a38ea7c35e8c1cd02af6ce7d17ac53
Tim Tanasse <tim.tanasse@gmail.com>
2016-03-13 12:32:41 -0700

    Changed handling for commands

--- src/rocketlog/processors/RegexProcessor.go
+++ src/rocketlog/processors/RegexProcessor.go
@@ -8,64 +8,61 @@ import (
 	"os/exec"
 )
 
-const MAPPING_REGEX = "(`.*`|\\([0-9]*\\))"
+const MAPPING_REGEX = "(\\([0-9]*\\))"
+const SHELL_REGEX = "(`.*`)"
 
 type RegexProcessor struct {
 	regex         *regexp.Regexp
 	mapping_regex *regexp.Regexp
+	shell_regex   *regexp.Regexp
 	mapping       string
 }
 
 func NewRegexProcessor(parser_regex, mapping string) *RegexProcessor{
 	regex := regexp.MustCompile(parser_regex)
 	mapping_regex := regexp.MustCompile(MAPPING_REGEX)
+	shell_regex := regexp.MustCompile(SHELL_REGEX)
 
 	reg := &RegexProcessor{
 		mapping: mapping,
 		regex: regex,
 		mapping_regex: mapping_regex,
+		shell_regex: shell_regex,
 	}
 
 	return reg
 }
 
 func (self *RegexProcessor) Process(input string) string {
-	var current_mapping_token string
-	var current_result_token string
 
 	result := self.regex.FindStringSubmatch(input)
 	mapping_result := self.mapping_regex.FindAllStringSubmatch(self.mapping, -1)
-	log.Print(mapping_result)
-	log.Print(result)
+	shell_result := self.shell_regex.FindAllStringSubmatch(self.mapping, -1)
+
 	output := self.mapping
 
 	for _, v := range mapping_result {
-		log.Print(v[0])
-		current_mapping_token = v[0]
-		current_result_token = self.generateResultToken(v, result)
+		current_mapping_token := v[1]
+		result_index, err := strconv.Atoi(v[2])
+		if(err != nil){
+			log.Fatal(err)
+		}
+
+		current_result_token := result[result_index]
+
+		output = strings.Replace(output, current_mapping_token, current_result_token, -1)
+	}
+
+	for _, v:= range shell_result{
+		current_mapping_token := v[1]
+		cmd_slices := strings.Split(v[2], " ")
+
+		current_result_token, err := exec.Command(cmd_slices[0], cmd_slices[1:]...).Output()
+		if(err != nil){
+			log.Fatal(err)
+		}
 		output = strings.Replace(output, current_mapping_token, current_result_token, -1)
 	}
 
 	return output
-}
-
-func (self *RegexProcessor) generateResultToken(input[]string, result []string) string{
-	var current_result_token string
-	if (input[0][0] == '`') {
-		cmd_slices := strings.Split(input[1][1:len(input)], " ")
-		cmd_result, err := exec.Command(cmd_slices[0], cmd_slices[1:]...).Output()
-		if(err != nil){
-			log.Fatal(err)
-		}
-		current_result_token = string(cmd_result)
-
-	}else{
-		result_index, err := strconv.Atoi(input[1][1:(len(input))])
-		if(err != nil){
-			log.Fatal(err)
-		}
-		current_result_token = result[result_index]
-	}
-	return current_result_token
-
 }
