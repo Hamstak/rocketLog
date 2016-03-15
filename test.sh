bba972c0ed5585b308251696dda0a03bb6eb3c6c
Tim Tanasse <tim.tanasse@gmail.com>
2016-03-14 19:25:44 -0700

    Have to use a script to properly implement code coverage for multiple packages

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-go test rocketlog/...
+set -e
+echo "" > coverage.txt
+
+for d in $(find ./* -maxdepth 10 -type d); do
+    if ls $d/*.go &> /dev/null; then
+        go test -coverprofile=profile.out -covermode=atomic $d
+        if [ -f profile.out ]; then
+            cat profile.out >> coverage.txt
+            rm profile.out
+        fi
+    fi
+done
